07d723da500a32d11d0c0a795063959ec10059d9
Jim Pedersen <66628138+Morfindien@users.noreply.github.com>
2020-09-30 17:44:29 +0200

    Add files via upload

---
+++ without spaces.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+string = input("random string with spaces: ")
+
+with_out_spaces = string.replace(" ", "")
+
+print(with_out_spaces)
+
+
+# In[ ]:
+
+
+
+
